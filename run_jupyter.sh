84b896f954e4670d5465f6b8cd79ee176c6f4290
karinait <karina.in.it@gmail.com>
2025-01-03 10:22:50 -0500

    Training final model on the full dataset

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -3,5 +3,8 @@
 # Adjust memory allocation strategy
 export TF_GPU_ALLOCATOR=cuda_malloc_async
 
+# Enable XLA to use available XLA devices for optimized computations 
+export TF_XLA_FLAGS="--tf_xla_enable_xla_devices"
+
 # Execute Jupyter Notebook using pipenv
 pipenv run jupyter notebook
